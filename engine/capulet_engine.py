0941c8bb415020b82b750176179204550f87cd09
Moses Ngige <moses.soft.eng@gmail.com>
2023-10-08 23:55:34 +0300

    Add types to class variables

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -20,8 +20,8 @@ class CapuletEngine(Engine):
             last_service_mileage (int): The mileage when the engine was last
                 serviced.
         """
-        self.current_mileage = current_mileage
-        self.last_service_mileage = last_service_mileage
+        self.current_mileage: int = current_mileage
+        self.last_service_mileage: int = last_service_mileage
 
     def needs_service(self) -> bool:
         """
