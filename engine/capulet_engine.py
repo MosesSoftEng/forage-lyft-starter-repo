321fa18328d5decb4ac12be571588766671fc782
Moses Ngige <moses.soft.eng@gmail.com>
2023-10-08 23:42:47 +0300

    Refactor CapuletEngine to use Engine interface

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,13 +1,33 @@
-from abc import ABC
-
-from car import Car
+from engine.engine_interface import Engine
 
 
-class CapuletEngine(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
+class CapuletEngine(Engine):
+    """
+    Represents a Capulet engine that implements the Engine interface.
+
+    Args:
+        current_mileage (int): The current mileage of the engine.
+        last_service_mileage (int): The mileage when the engine was last
+            serviced.
+    """
+
+    def __init__(self, current_mileage: int, last_service_mileage: int):
+        """
+        Initialize a CapuletEngine object.
+
+        Args:
+            current_mileage (int): The current mileage of the engine.
+            last_service_mileage (int): The mileage when the engine was last
+                serviced.
+        """
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
+    def needs_service(self) -> bool:
+        """
+        Determines if the Capulet engine needs servicing.
+
+        Returns:
+            bool: True if the engine needs servicing, False otherwise.
+        """
         return self.current_mileage - self.last_service_mileage > 30000
