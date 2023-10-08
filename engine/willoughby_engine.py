0165bad1690614314de2ec5738bb30e3335acfa2
Moses Ngige <moses.soft.eng@gmail.com>
2023-10-08 23:54:41 +0300

    Refactor Willoughby engine to implements Engine interface

--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -1,13 +1,33 @@
-from abc import ABC
-
-from car import Car
+from engine.engine_interface import Engine
 
 
-class WilloughbyEngine(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
-        self.current_mileage = current_mileage
-        self.last_service_mileage = last_service_mileage
+class WilloughbyEngine(Engine):
+    """
+    Represents a Willoughby engine that implements the Engine interface.
 
-    def engine_should_be_serviced(self):
+    Args:
+        current_mileage (int): The current mileage of the engine.
+        last_service_mileage (int): The mileage when the engine was last
+            serviced.
+    """
+
+    def __init__(self, current_mileage: int, last_service_mileage: int):
+        """
+        Initialize a WilloughbyEngine object.
+
+        Args:
+            current_mileage (int): The current mileage of the engine.
+            last_service_mileage (int): The mileage when the engine was last
+                serviced.
+        """
+        self.current_mileage: int = current_mileage
+        self.last_service_mileage: int = last_service_mileage
+
+    def engine_should_be_serviced(self) -> bool:
+        """
+        Determines if the Willoughby engine should be serviced.
+
+        Returns:
+            bool: True if the engine should be serviced, False otherwise.
+        """
         return self.current_mileage - self.last_service_mileage > 60000
