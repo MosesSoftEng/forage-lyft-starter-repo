c0c66185c248de1f8fe68d9759ce83eb759c90b4
Moses Ngige <moses.soft.eng@gmail.com>
2023-10-29 14:31:03 +0300

    Improve docs

--- tests/test_engines/test_capulet_engine.py
+++ tests/test_engines/test_capulet_engine.py
@@ -4,36 +4,37 @@ from engine.capulet_engine import CapuletEngine
 
 
 class TestCapuletEngine(unittest.TestCase):
-	def test_engine_should_be_serviced(self) -> None:
-		"""
-		Test case to verify if the engine should be serviced based if current mileage exceeds service limit threshold.
+    def test_engine_should_be_serviced(self) -> None:
+        """
+        Test engine should be serviced when current mileage exceeds service limit threshold.
 
-		Parameters:
-		- self: The test case instance.
+        Parameters:
+        - self: The test case instance.
 
-		Returns:
-		- None
-		"""
-		current_mileage:int = 30001
-		last_service_mileage:int = 0
+        Returns:
+        - None
+        """
+        current_mileage: int = 30001
+        last_service_mileage: int = 0
 
-		engine: CapuletEngine = CapuletEngine(current_mileage, last_service_mileage)
-		self.assertTrue(engine.needs_service())
+        engine: CapuletEngine = CapuletEngine(
+            current_mileage, last_service_mileage)
+        self.assertTrue(engine.needs_service())
 
-	def test_engine_should_not_be_serviced(self) -> None:
-		"""
-		Test case to verify that the engine should not be serviced when the current mileage is below than service
-			limit threshold.
+    def test_engine_should_not_be_serviced(self) -> None:
+        """
+        Test engine should not be serviced when current mileage is below service limit threshold.
 
-		:param self: The test case instance.
-		:return: None
-		"""
-		current_mileage:int = 30000
-		last_service_mileage:int = 0
+        :param self: The test case instance.
+        :return: None
+        """
+        current_mileage: int = 30000
+        last_service_mileage: int = 0
 
-		engine: CapuletEngine = CapuletEngine(current_mileage, last_service_mileage)
-		self.assertFalse(engine.needs_service())
+        engine: CapuletEngine = CapuletEngine(
+            current_mileage, last_service_mileage)
+        self.assertFalse(engine.needs_service())
 
 
 if __name__ == "main":
-	unittest.main()
+    unittest.main()
